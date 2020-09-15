bbb7735e40fb71dbe6a52e0c2b8f37c5f5cc7cf4
Mark Bowler <mark.bowler@mhr.co.uk>
2020-09-15 17:15:48 +0100

    Initial commit

---
+++ Dude.AutomationFunctionApp/DevopsAuth.cs
@@ -0,0 +1,36 @@
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Dude.AutomationFunctionApp
+{
+    public static class DevopsAuth
+    {
+        private static readonly HttpClient Client = new HttpClient();
+        
+        [FunctionName("DevopsAuth")]
+        public static async Task<IActionResult> RunAsync(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]
+            HttpRequest req, ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            const string url =
+                "https://dev.azure.com/mhrengineering/Trent11%20Hosted/_apis/git/repositories/PF.Automation/items?path=%2FDeploymentSpecific%2FSubprocesses%2FAppRegSecretExpiry.ps1&api-version=5.1";
+
+            string pat = req.Query["pat"];
+            var token = System.Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes("dude:" + pat));
+            
+            Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", token);
+
+            var response = Client.GetAsync(url).Result;
+            
+            return new OkObjectResult(response.Content.ReadAsStringAsync().Result);
+        }
+    }
+}
